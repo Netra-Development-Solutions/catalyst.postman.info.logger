4a26c287dd6fefed00782f732e36d8f7e9ba0b5a
Naman Khater <naman22khater@gmail.com>
2024-04-14 00:02:00 +0530

    log add API added

---
+++ models/LogInfo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const LogInfoSchema = new mongoose.Schema({
+    logType: {
+        type: String,
+        required: true
+    },
+    logMessage: {
+        type: String
+    },
+    logDetails: {
+        type: String
+    },
+    logLevel: {
+        type: String,
+        required: true
+    },
+    logSource: {
+        type: String,
+        required: true
+    },
+    logUser: {
+        type: String,
+        required: true,
+        ref: 'User'
+    },
+    logEnv: {
+        type: String,
+        required: true
+    },
+    logApp: {
+        type: String,
+        required: true
+    },
+    transactionId: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('LogInfo', LogInfoSchema);
